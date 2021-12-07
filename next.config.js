20bac10c7e7fc94efb926eedb2fd2dddaa7a156e
Matheus <ferreiramatheus48@gmail.com>
2021-12-06 21:55:52 -0300

    feat: update readme

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
+/** @type {import('next').NextConfig} */
 const path = require("path");
 const withSass = require("@zeit/next-sass");
 
-/** @type {import('next').NextConfig} */
 module.exports = withSass({
   cssModules: true,
 });
